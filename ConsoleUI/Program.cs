a41e52746dc92c68b94bfd3ab0e9ed7ed535a050
CemalFDogan <cxexmxaxl4409@gmail.com>
2022-08-26 10:51:05 +0300

    Gun8 ---> bitti

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,14 +1,20 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using System;
+using System.Linq;
 
 namespace ConsoleUI
 {
+    // SOLID
+    // Open Closed Principle
     class Program
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+
+
+            ProductManager productManager = new ProductManager(new EfProductDal());
 
             foreach (var product in productManager.GetAll())
             {
