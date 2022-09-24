cd29eb1167c9713875db53c72327793d9e6a4907
CemalFDogan <cxexmxaxl4409@gmail.com>
2022-09-24 11:08:57 +0300

    11. Gün -  WebAPI

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -14,8 +14,11 @@ namespace Business.Abstract
         IDataResult<List<Product>> GetAll();
         IDataResult<List<Product>> GetAllByCategoryId(int id);
         IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max);
+        
         IDataResult<List<ProductDetailDto>> GetProductDetails();
 
         IResult Add(Product product);
+
+        // RESTFUL --> HTTP --> 
     }
 }
